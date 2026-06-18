44057fccba3d241cf8c1e9d43678926d309a6b16
Akila Kasun <akilakasun10769@gmail.com>
2026-06-19 01:58:50 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tree-house-villa/',
+  base: "/tree-house-villa/",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", 
